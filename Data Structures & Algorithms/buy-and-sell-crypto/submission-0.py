b0adc91f9ef86a60daea0ff67b98f88c489274b2
Emilia.jy <98992915+Emilia0608@users.noreply.github.com>
2026-05-19 20:41:48 +0900

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,13 @@
+# 탐색 인덱스를 돌면서 탐색 인덱스보다 큰 인덱스 값을 가진 데이터 중 수익을 낼 수 있는지 체크
+# 탐색 인덱스, 비교할 인덱스, 수익
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        for idx, price in enumerate(prices):
+            for i in range(idx+1, len(prices)):
+                if prices[i] - price > max_profit:
+                    max_profit = prices[i] - price
+        return max_profit
+
+        
