f39d1267400af233c3b541ffcb02ce53b6825c1f
Emilia.jy <98992915+Emilia0608@users.noreply.github.com>
2026-05-20 23:10:37 +0900

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,22 @@
+# 탐색 인덱스를 옮기면서 왼쪽 배열 곱 저장, 오른쪽 배열 곱 저장
+# 풀다 포기함
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [1] * len(nums)
+
+        left = 1
+        for i in range(len(nums)):
+            answer[i] = left
+            left *= nums[i]
+
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            answer[i] *= right
+            right *= nums[i]
+
+        return answer
+
+
+
+        
